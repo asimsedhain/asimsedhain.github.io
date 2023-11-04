ad949aa732606a902e5e306012c24b677d924645
asimsedhain <asimsedhain33@gmail.com>
2023-11-04 15:24:51 -0500

    feat: add favicon icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,13 @@
 	import Year from "$lib/components/Year.svelte";
 </script>
 
+<svelte:head>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐸</text></svg>"
+	/>
+</svelte:head>
+
 <NavBar />
 
 <main id="top">
