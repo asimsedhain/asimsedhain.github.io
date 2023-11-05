e459575ef675b9df606f92322c64f79eaf9eca4e
asimsedhain <asimsedhain33@gmail.com>
2023-11-04 23:10:02 -0500

    feat: add notes component

---
+++ src/lib/components/Note.svelte
@@ -0,0 +1,18 @@
+<div class="border-l-4 border-sky-600 my-4 pl-4">
+	<div class="font-bold text-sky-600 mb-2 flex">
+		<svg
+			class="mr-2 stroke-sky-600 fill-sky-600 my-auto"
+			viewBox="0 0 16 16"
+			version="1.1"
+			width="16"
+			height="16"
+			aria-hidden="true"
+			><path
+				d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
+			/></svg
+		>
+		Note
+	</div>
+
+	<slot />
+</div>
