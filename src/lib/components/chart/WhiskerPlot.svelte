b9060acfa54729e52f0ef38e711b8bae711438d6
asimsedhain <asimsedhain33@gmail.com>
2023-11-26 17:32:35 -0600

    feat: add Tooltip

--- src/lib/components/chart/WhiskerPlot.svelte
+++ src/lib/components/chart/WhiskerPlot.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-	import { LayerCake, Svg } from "layercake";
+	import type { Measurement } from "$lib/types";
+	import { LayerCake, Svg, Html } from "layercake";
 	import { scaleBand } from "d3-scale";
 	import AxisX from "./AxisX.svg.svelte";
 	import AxisY from "./AxisY.svg.svelte";
 	import Whisker from "./Whisker.svg.svelte";
+	import Tooltip from "./WhiskerTooltip.html.svelte";
 
 	export let data: Measurement[];
 
-	const y: (keyof Measurement)[] = ["median", "mean", "std", "max", "min"];
+	const y: (keyof Measurement)[] = ["median", "mean", "stddev", "max", "min"];
+	$: yDomain = [
+		Math.min(...data.map((d) => d.min)),
+		Math.max(...data.map((d) => d.max)),
+	];
 </script>
 
 <div class="w-full h-80 mb-16 py-5">
@@ -18,8 +24,13 @@
 		xScale={scaleBand().paddingInner(0.028).round(true)}
 		xDomain={data.map((entry) => entry.group)}
 		yPadding={[20, 20]}
+		{yDomain}
 		{data}
+		debug={true}
 	>
+		<Html>
+			<Tooltip />
+		</Html>
 		<Svg>
 			<AxisX gridlines={true} baseline={true} />
 			<AxisY gridlines={true} />
---
+++ src/lib/components/chart/WhiskerTooltip.html.svelte
@@ -0,0 +1,85 @@
+<!--
+  @component
+  Generates a hover tooltip. It creates a slot with an exposed variable via `let:detail` that contains information about the event. Use the slot to populate the body of the tooltip using the exposed variable `detail`.
+ -->
+<script lang="ts">
+	import { getContext } from "svelte";
+	import type { Measurement } from "$lib/types";
+
+	// @ts-ignore
+	const { data, xGet, yGet, xScale } = getContext("LayerCake");
+
+	let selectedDataPoint: Measurement | null = null;
+
+	/** @type {Number} [offset=-35] - A y-offset from the hover point, in pixels. */
+	export let offset: number = -15;
+	$: isBandwidth = typeof $xScale.bandwidth === "function";
+
+	function formatValue(value: number | string) {
+		if (typeof value === "string") {
+			return value;
+		}
+		return value.toFixed(2);
+	}
+</script>
+
+{#if selectedDataPoint}
+	{@const xPoint = isBandwidth
+		? $xScale.bandwidth() / 2 + $xGet(selectedDataPoint)
+		: $xGet(selectedDataPoint)}
+	{@const [median, _mean, _std, _max, _min] = $yGet(selectedDataPoint)}
+	<div
+		class="tooltip"
+		style="
+      top:{median + offset}px;
+      left:{xPoint}px;
+    "
+	>
+		{#each Object.entries(selectedDataPoint) as [key, value]}
+			<div>
+				<span class="font-bold">{key}:</span>
+				{formatValue(value)}
+			</div>
+		{/each}
+	</div>
+{/if}
+
+{#each $data as d}
+	{@const xPoint = isBandwidth
+		? $xScale.bandwidth() / 2 + $xGet(d)
+		: $xGet(d)}
+	{@const [median, _mean, _std, _max, _min] = $yGet(d)}
+	<div
+		class="hover-points"
+		style="
+		width: {Math.abs(offset)}px;
+		height: {Math.abs(offset)}px;
+		top:{median + offset}px;
+        left:{xPoint}px;
+    "
+		on:mouseenter={(_e) => {
+			selectedDataPoint = d;
+		}}
+		on:mouseleave={(_e) => {
+			selectedDataPoint = null;
+		}}
+	/>
+{/each}
+
+<style>
+	.tooltip {
+		position: absolute;
+		width: 180px;
+		border: 1px solid #ccc;
+		font-size: 13px;
+		background: rgba(255, 255, 255, 0.85);
+		transform: translate(-50%, -100%);
+		padding: 5px;
+		z-index: 15;
+	}
+	.hover-points {
+		position: absolute;
+		z-index: 20;
+		transform: translateX(-50%) translateY(50%);
+	}
+</style>
