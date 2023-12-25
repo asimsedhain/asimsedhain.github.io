17cc136177f5cb7619ed506d8623a8043b84b9b7
asimsedhain <asimsedhain33@gmail.com>
2023-12-24 21:04:41 -0600

    feat: add legend for Whisker plot

---
+++ src/lib/components/chart/WhiskerLegend.html.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { getContext } from "svelte";
+	import type { Measurement } from "$lib/types";
+
+	// @ts-ignore
+	const { data, xGet, yGet, xScale } = getContext("LayerCake");
+
+	let selectedDataPoint: Measurement | null = null;
+
+	/** @type {Number} [offset=-15] - A y-offset from the hover point, in pixels. */
+	export let offset: number = -15;
+	$: isBandwidth = typeof $xScale.bandwidth === "function";
+
+	function formatValue(value: number | string) {
+		if (typeof value === "string") {
+			return value;
+		}
+		return value.toFixed(2);
+	}
+</script>
