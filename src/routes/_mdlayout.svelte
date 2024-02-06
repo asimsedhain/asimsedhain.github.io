5bdb1f3296a62c361d16a55900a040cbd8913316
asimsedhain <asimsedhain33@gmail.com>
2024-02-05 23:46:17 -0600

    fix: annotate _mdlayout with jsdocs

--- src/routes/_mdlayout.svelte
+++ src/routes/_mdlayout.svelte
@@ -1,13 +1,18 @@
-<script lang="ts">
-	// @ts-expect-error: type for title is assigned by sveltekit
-	export let title: string
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let title
 
-	export let date: string
+	/**
+	 * @type {string}
+	 */
+	export let date
 </script>
 
 <svelte:head>
 	<title>{title ? title + " - " : ""}Ashim Sedhain</title>
 </svelte:head>
 
-<h5 class='md-5 mt-5 dark:text-cyan-50 text-gray-500'>{date}</h5>
+<h5 class="md-5 mt-5 dark:text-cyan-50 text-gray-500">{date}</h5>
 <slot />
